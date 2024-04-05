ef8fb8aa995b3537feba8ba1846a5168af2a6cd9
Arthuthu <arthurgeromello@hotmail.com>
2024-04-05 18:10:50 -0300

    Add documentation and updated response types for subscription api

--- src/SubscriptionService/SubscriptionApi/Controllers/v1/SubscriptionController.cs
+++ src/SubscriptionService/SubscriptionApi/Controllers/v1/SubscriptionController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using SubscriptionApi.Dtos.Requests;
+using SubscriptionApi.Dtos.Responses;
 using SubscriptionApplication.Abstractions.AppServices;
 using SubscriptionDomain.Entities;
 
@@ -19,24 +20,29 @@ namespace SubscriptionApi.Controllers.v1
 
         [HttpPost]
         [Route("v1/subscription/add")]
-        public async Task<IActionResult> Add([FromBody] SubscriptionRequest request, CancellationToken cancellationToken)
+		[ProducesResponseType(typeof(SubscriptionResponse), 200),
+		 ProducesResponseType(500)]
+		public async Task<IActionResult> Add([FromBody] SubscriptionRequest request, CancellationToken cancellationToken)
         {
             try
             {
                 Subscription subscription = _mapper.Map<Subscription>(request);
                 await _appService.Add(subscription, cancellationToken);
 
-                return Ok(subscription);
+                return Ok(_mapper.Map<SubscriptionResponse>(subscription));
             }
             catch (Exception ex)
             {
-                return BadRequest($"An error ocurred while adding the subscription {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    $"An error ocurred while adding the subscription {ex.Message}");
             }
         }
 
         [HttpGet]
         [Route("v1/subscription/get/{id}")]
-        public async Task<IActionResult> Get(Guid id, CancellationToken cancellationToken)
+		[ProducesResponseType(typeof(SubscriptionResponse),200),
+		 ProducesResponseType(404), ProducesResponseType(500)]
+		public async Task<IActionResult> Get(Guid id, CancellationToken cancellationToken)
         {
             try
             {
@@ -47,17 +53,20 @@ namespace SubscriptionApi.Controllers.v1
                     return NotFound("Subscription not found");
                 }
 
-                return Ok(subscription);
+                return Ok(_mapper.Map<SubscriptionResponse>(subscription));
             }
             catch (Exception ex)
             {
-                return BadRequest($"An error has occurred while searching for the subscription: {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, 
+                    $"An error has occurred while searching for the subscription: {ex.Message}");
             }
         }
 
         [HttpGet]
         [Route("v1/subscription/get")]
-        public async Task<IActionResult> Get(CancellationToken cancellationToken)
+		[ProducesResponseType(typeof(List<SubscriptionResponse>), 200),
+		 ProducesResponseType(404), ProducesResponseType(500)]
+		public async Task<IActionResult> Get(CancellationToken cancellationToken)
         {
             try
             {
@@ -68,18 +77,21 @@ namespace SubscriptionApi.Controllers.v1
                     return NotFound("No subscriptions has been found");
                 }
 
-                return Ok(subscriptions);
+                return Ok(_mapper.Map<List<SubscriptionResponse>>(subscriptions));
 
             }
             catch (Exception ex)
             {
-                return BadRequest($"An error has occurred while searching for the subscription: {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, 
+                    $"An error has occurred while searching for the subscription: {ex.Message}");
             }
         }
 
         [HttpPut]
         [Route("v1/subscription/update")]
-        public async Task<IActionResult> Update([FromBody] SubscriptionRequest request, CancellationToken cancellationToken)
+		[ProducesResponseType(typeof(SubscriptionResponse), 200),
+		 ProducesResponseType(404), ProducesResponseType(500)]
+		public async Task<IActionResult> Update([FromBody] SubscriptionRequest request, CancellationToken cancellationToken)
         {
             try
             {
@@ -91,17 +103,20 @@ namespace SubscriptionApi.Controllers.v1
                     return NotFound("An error has occurred while updating the subscription, subscription not found");
                 }
 
-                return Ok(result);
+                return Ok(_mapper.Map<SubscriptionResponse>(result));
             }
             catch (Exception ex)
             {
-                return BadRequest($"An error has occurred while updating the subscription: {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, 
+                    $"An error has occurred while updating the subscription: {ex.Message}");
             }
         }
 
         [HttpDelete]
         [Route("v1/subscription/delete/{id}")]
-        public async Task<IActionResult> Delete(Guid id, CancellationToken cancellationToken)
+		[ProducesResponseType (200),
+		 ProducesResponseType(404), ProducesResponseType(500)]
+		public async Task<IActionResult> Delete(Guid id, CancellationToken cancellationToken)
         {
             try
             {
@@ -116,7 +131,8 @@ namespace SubscriptionApi.Controllers.v1
             }
             catch (Exception ex)
             {
-                return BadRequest($"An error has occurred while deleting the subscription: {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, 
+                    $"An error has occurred while deleting the subscription: {ex.Message}");
             }
         }
     }

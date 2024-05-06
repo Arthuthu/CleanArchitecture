db98297fce794fcfe73f5a6b5ffb2c636a1c076d
Arthuthu <arthurgeromello@hotmail.com>
2024-05-06 14:23:42 -0300

    Add exercicio service

---
+++ src/TreinoService/TreinoApplication/Services/ExercicioService.cs
@@ -0,0 +1,41 @@
+﻿using TreinoApplication.Abstractions;
+using TreinoDomain.Entities;
+using TreinoInfra.Repositories;
+
+namespace TreinoApplication.Services
+{
+    public sealed class ExercicioService : IExercicioService
+	{
+		private readonly IExercicioRepository _repository;
+
+		public ExercicioService(IExercicioRepository repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task<Exercicio> Add(Exercicio exercicio, CancellationToken cancellationToken)
+		{
+			return await _repository.Add(exercicio, cancellationToken);
+		}
+
+		public async Task<Exercicio?> Get(Guid id, CancellationToken cancellationToken)
+		{
+			return await _repository.Get(id, cancellationToken);
+		}
+
+		public async Task<List<Exercicio>> Get(CancellationToken cancellationToken)
+		{
+			return await _repository.Get(cancellationToken);
+		}
+
+		public async Task<Exercicio?> Update(Exercicio exercicio, CancellationToken cancellationToken)
+		{
+			return await _repository.Update(exercicio, cancellationToken);
+		}
+
+		public async Task<bool> Delete(Guid id, CancellationToken cancellationToken)
+		{
+			return await _repository.Delete(id, cancellationToken);
+		}
+	}
+}

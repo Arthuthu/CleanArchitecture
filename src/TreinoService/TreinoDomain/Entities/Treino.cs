4ae0d7a310e6ff8a031607830948379d8d36928c
Arthuthu <arthurgeromello@hotmail.com>
2024-05-13 14:10:03 -0300

    Fixed treinos migration bug

--- src/TreinoService/TreinoDomain/Entities/Treino.cs
+++ src/TreinoService/TreinoDomain/Entities/Treino.cs
@@ -4,6 +4,7 @@ namespace TreinoDomain.Entities
 {
 	public sealed class Treino : Entity
 	{
+		public Guid UserId { get; set; }
 		public string? Nome { get; set; }
 		public List<Exercicio>? Exercicios { get; set; }
 	}
--- src/TreinoService/TreinoInfra/Migrations/20240513162249_AddUserIdOnTreino.Designer.cs
+++ src/TreinoService/TreinoInfra/Migrations/20240513162249_AddUserIdOnTreino.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TreinoInfra.Context;
 
@@ -11,9 +12,11 @@ using TreinoInfra.Context;
 namespace TreinoInfra.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class TreinoContextModelSnapshot : ModelSnapshot
+    [Migration("20240513162249_AddUserIdOnTreino")]
+    partial class AddUserIdOnTreino
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

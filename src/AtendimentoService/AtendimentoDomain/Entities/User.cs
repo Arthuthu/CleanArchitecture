6a5580f4a7ee7f85d471102f937d9e8bdf77ec45
Arthur <arthurgeromello@hotmail.com>
2024-08-01 20:56:51 -0300

    Atendimento repository

---
+++ src/AtendimentoService/AtendimentoDomain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using UserDomain.Primitives;
+
+namespace UserDomain.Entities
+{
+	public class User : Entity
+	{
+		public string? Username { get; set; }
+		public string? Email { get; set; }
+		public string? Password { get; set; }
+
+		public Guid? SubscriptionId { get; set; }
+	}
+}
---
+++ src/AtendimentoService/AtendimentoInfra/Repositories/AtendimentoRepository.cs
@@ -0,0 +1,42 @@
+﻿using AtendimentoDomain.Entities;
+using AtendimentoInfra.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace AtendimentoInfra.Repositories
+{
+    public sealed class AtendimentoRepository
+    {
+        private readonly AtendimentoContext _context;
+
+        public AtendimentoRepository(AtendimentoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Atendimento?> GetAtendimentosById(Guid atendimentoId, CancellationToken ct)
+        {
+            Atendimento? atendimento = await _context.Atendimento.FindAsync(atendimentoId, ct);
+            return atendimento;
+        }
+
+        public async Task<List<Atendimento>?> GetAtendimentosByUserId(Guid userId, CancellationToken ct)
+        {
+            List<Atendimento> atendimentos = await _context.Atendimento.Where(x => x.UserId == userId).ToListAsync(ct);
+            return atendimentos;
+        }
+
+        public async Task<Atendimento> Add(Atendimento atendimento, CancellationToken ct)
+        {
+            await _context.AddAsync(atendimento, ct);
+            await _context.SaveChangesAsync(ct);
+
+            return atendimento;
+        }
+
+        public async Task<bool> Delete(Guid id, CancellationToken ct)
+        {
+            int affectedRows = await _context.Atendimento.Where(x => x.Id == id).ExecuteDeleteAsync(ct);
+            return affectedRows > 0;
+        }
+    }
+}
